2e7db4fb05e5b0e0531686bec12196676966084a
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-03-22 11:13:23 +0530

    login and signup routes

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,12 +1,58 @@
 import User from "../models/Users.models.js";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
-const register =  async (req,res) => {
-    const {name} = req.body;
-    const user = await User.create({
-   name,
-    });
 
-    res.json(user);
+const register =async (req,res) => {
+    try {
+        const { name, email, password } = req.body;
 
-}
-export default register;
+        const hashedpassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedpassword,
+        });
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+const login = async(req,res) => {
+      try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+
+        
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.json({
+            message: "Login successful",
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
