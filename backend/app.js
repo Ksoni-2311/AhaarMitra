3351e55ae3326d83ded122819e57e4326d2b0436
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-04-02 12:33:03 +0530

    otp verfiy

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 import dotenv from "dotenv";
+import OTP from './routes/otp.js';
+import vendorRoutes from './routes/vendor.routes.js';
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,7 @@ app.use(cors());
 
 app.use("/api/user",userRoutes);
 app.use("/api/vendor",vendorRoutes);
+
 
 
 app.get('/',(req,res) => {
